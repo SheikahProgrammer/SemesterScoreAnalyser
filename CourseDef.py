c988ac841fd809cc2d623c810d318e4106de34fc
Avinash Nair <avinashnairofficialz@gmail.com>
2023-03-20 02:08:11 +0530

    Added Lab

--- CourseDef.py
+++ CourseDef.py
@@ -23,3 +23,18 @@ class CourseTheory:
 #EXAMPLE = CourseTheory('BEXP101L',[50,50],100,[10,10,10])
 #EXAMPLE.ShowScore()
 #EXAMPLE.Save()
+
+class CourseLab:
+    def __init__(self,code, fat, ias):
+        self.code = code
+        self.fat = fat
+        self.ias = ias
+        self.score = ((((self.fat)/100)*0.4)+ (sum(self.ias)/(len(self.ias)*10))*0.6)*100
+    def ShowScore(self):
+        print(str(self.score)+" %")
+    def Save(self):
+        with open('Lab.csv', 'a', newline='') as file:
+            csv.writer(file).writerow([self.code, BLTS(self.ias),self.fat ,self.score])
+
+EXAMPLE = CourseLab('BEXP101P',100,[10,10,10,10,5])
+EXAMPLE.Save()
