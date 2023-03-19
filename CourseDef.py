0c15f00edc2736ae907e267a2e99c4f550cbf588
Avinash Nair <avinashnairofficialz@gmail.com>
2023-03-20 01:58:53 +0530

    Thoery Done

--- CourseDef.py
+++ CourseDef.py
@@ -1,5 +1,11 @@
 import csv
 
+def BLTS(lst):
+    string = str(lst)
+    string=string.replace('[','')
+    string=string.replace(']','')         
+    string=string.replace(', ',':') 
+    return string
 
 class CourseTheory:
     def __init__(self,code, cats, fat, ias):
@@ -7,9 +13,13 @@ class CourseTheory:
         self.cats = cats
         self.fat = fat
         self.ias = ias
-        self.score = ((self.fat)/100)*0.4+(sum(self.cats)/(50*len(self.cats))*0.3)+ (sum(self.ias)/(len(self.ias)*10))*0.3
+        self.score = (((self.fat)/100)*0.4+(sum(self.cats)/(50*len(self.cats))*0.3)+ (sum(self.ias)/(len(self.ias)*10))*0.3)*100
     def ShowScore(self):
-        print(str(self.score*100)+" %")
+        print(str(self.score)+" %")
+    def Save(self):
+        with open('Theory.csv', 'a', newline='') as file:
+            csv.writer(file).writerow([self.code, BLTS(self.cats), BLTS(self.ias),self.fat ,self.score]) 
 
 #EXAMPLE = CourseTheory('BEXP101L',[50,50],100,[10,10,10])
 #EXAMPLE.ShowScore()
+#EXAMPLE.Save()
